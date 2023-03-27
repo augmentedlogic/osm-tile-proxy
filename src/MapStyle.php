baa014a9905373d5f392383b05eec7b225e0b71b
Wolfgang Hauptfleisch <wh@augmentedlogic.com>
2023-03-27 19:10:31 +0100

    image type conversion and fixes

--- src/MapStyle.php
+++ src/MapStyle.php
@@ -1,25 +1,25 @@
 <?php
 /**
-Copyright (c) 2020 Wolfgang Hauptfleisch/augmentedlogic <dev@augmentedlogic.com>
+  Copyright (c) 2020 Wolfgang Hauptfleisch/augmentedlogic <dev@augmentedlogic.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-**/
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ **/
 namespace com\augmentedlogic\osmtileproxy;
 
 use Imagick;
@@ -27,137 +27,169 @@ use Imagick;
 class MapStyle
 {
 
-	private $stylename;
-	private $option_modulate = false;
+    const IMAGE_FORMAT_JPEG = 1;
+    const IMAGE_FORMAT_PNG = 2;
+    const IMAGE_FORMAT_WEBP = 3;
 
-	private $option_modulate_brightness = 100;
-	private $option_modulate_saturation = 100;
-	private $option_modulate_hue = 100;
+    private $stylename;
+    private $option_modulate = false;
 
-	private $option_sepia = false;
-	private $option_sepia_value;
+    private $option_modulate_brightness = 100;
+    private $option_modulate_saturation = 100;
+    private $option_modulate_hue = 100;
 
-	private $option_negate = false;
-	private $option_negate_gray_only = false;
-	private $option_negate_channel;
+    private $option_sepia = false;
+    private $option_sepia_value;
 
-	private $mirrors = array();
-	private $refresh = 14;
+    private $option_negate = false;
+    private $option_negate_gray_only = false;
+    private $option_negate_channel;
 
-	private $option_lang = null;
+    private $option_image_format = "png";
+    private $option_image_mimetype = "image/png";
+    private $option_image_checktype = IMAGETYPE_PNG;
 
+    private $mirrors = array();
+    private $refresh = 14;
 
-	function __construct($stylename)
-	{
-		$this->stylename = $stylename;
-	}
-
-
-	public function setMirrors($mirrors)
-	{
-		$this->mirrors = $mirrors;
-	}
-
-	public function setLang($lang)
-	{
-		$this->lang = $lang;
-	}
-
-	public function setEffectModulate($brightness = 100, $saturation = 50, $hue = 100)
-	{
-		$this->option_modulate = true;
-		$this->option_modulate_brightness = $brightness;
-		$this->option_modulate_saturation = $saturation;
-		$this->option_modulate_hue = $hue;
-	}
-
-	public function setEffectSepia($value = 90)
-	{
-		$this->option_sepia = true;
-		$this->option_sepia_value = $value;
-	}
-
-	public function setEffectNegate($gray_only = false, $channel = Imagick::CHANNEL_DEFAULT)
-	{
-		$this->option_negate = true;
-		$this->option_negate_gray_only = $gray_only;
-		$this->option_negate_channel = $channel;
-	}
+    private $option_lang = null;
 
 
-	public function setRefresh()
-	{
-		$this->refresh = $refresh;
-	}
-
-	public function getRefresh()
-	{
-		return $this->refresh;
-	}
+    function __construct($stylename)
+    {
+        $this->stylename = $stylename;
+    }
 
 
-	public function getName()
-	{
-		return $this->stylename;
-	}
+    public function setMirrors($mirrors)
+    {
+        $this->mirrors = $mirrors;
+    }
+
+    public function setLang($lang)
+    {
+        $this->lang = $lang;
+    }
+
+    public function setEffectModulate($brightness = 100, $saturation = 50, $hue = 100)
+    {
+        $this->option_modulate = true;
+        $this->option_modulate_brightness = $brightness;
+        $this->option_modulate_saturation = $saturation;
+        $this->option_modulate_hue = $hue;
+    }
+
+    public function setEffectSepia($value = 90)
+    {
+        $this->option_sepia = true;
+        $this->option_sepia_value = $value;
+    }
+
+    public function setEffectNegate($gray_only = false, $channel = Imagick::CHANNEL_DEFAULT)
+    {
+        $this->option_negate = true;
+        $this->option_negate_gray_only = $gray_only;
+        $this->option_negate_channel = $channel;
+    }
 
 
-	public function getMirrors()
-	{
-		return $this->mirrors;
-	}
+    public function setRefresh()
+    {
+        $this->refresh = $refresh;
+    }
 
-	public function getModulate()
-	{
-		return $this->option_modulate;
-	}
-
-	public function getModulateBrightness()
-	{
-		return $this->option_modulate_brightness;
-	}
-
-	public function getModulateSaturation()
-	{
-		return $this->option_modulate_saturation;
-	}
-
-	public function getModulateHue()
-	{
-		return $this->option_modulate_hue;
-	}
-
-	public function getSepia()
-	{
-		return $this->option_sepia;
-	}
-
-	public function getSepiaValue()
-	{
-		return $this->option_sepia_value;
-	}
+    public function getRefresh()
+    {
+        return $this->refresh;
+    }
 
 
-	public function getNegate()
-	{
-		return $this->option_negate;
-	}
-
-	public function getNegateGrayOnly()
-	{
-		return $this->option_negate_gray_only;
-	}
-
-	public function getNegateChannel()
-	{
-		return $this->option_negate_channel;
-	}
-
-        public function getLang()
-        {
-                return $this->lang;
+    public function setImageFormat($image_format)
+    {
+        if($image_format == self::IMAGE_FORMAT_JPEG) {
+            $this->option_image_format = "jpg";
+            $this->option_image_mimetype = "image/jpeg";
         }
+        if($image_format == self::IMAGE_FORMAT_PNG) {
+            $this->option_image_format = "png";
+            $this->option_Image_mimetype = "image/png";
+        }
+        if($image_format == self::IMAGE_FORMAT_WEBP) {
+            $this->option_image_format = "webp";
+            $this->option_image_mimetype = "image/webp";
+        }
+    }
 
+    public function getImageFormat()
+    {
+        return $this->option_image_format;
+    }
+
+    public function getName()
+    {
+        return $this->stylename;
+    }
+
+
+    public function getMirrors()
+    {
+        return $this->mirrors;
+    }
+
+    public function getModulate()
+    {
+        return $this->option_modulate;
+    }
+
+    public function getModulateBrightness()
+    {
+        return $this->option_modulate_brightness;
+    }
+
+    public function getModulateSaturation()
+    {
+        return $this->option_modulate_saturation;
+    }
+
+    public function getModulateHue()
+    {
+        return $this->option_modulate_hue;
+    }
+
+    public function getSepia()
+    {
+        return $this->option_sepia;
+    }
+
+    public function getSepiaValue()
+    {
+        return $this->option_sepia_value;
+    }
+
+    public function getNegate()
+    {
+        return $this->option_negate;
+    }
+
+    public function getNegateGrayOnly()
+    {
+        return $this->option_negate_gray_only;
+    }
+
+    public function getNegateChannel()
+    {
+        return $this->option_negate_channel;
+    }
+
+    public function getImageChecktype()
+    {
+        return $this->option_image_checktype;
+    }
+
+    public function getLang()
+    {
+        return $this->option_lang;
+    }
 
 }
 
--- src/TileProxy.php
+++ src/TileProxy.php
@@ -1,25 +1,25 @@
 <?php
 /**
-Copyright (c) 2020 Wolfgang Hauptfleisch/augmentedlogic <dev@augmentedlogic.com>
+  Copyright (c) 2020 Wolfgang Hauptfleisch/augmentedlogic <dev@augmentedlogic.com>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-**/
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ **/
 namespace com\augmentedlogic\osmtileproxy;
 
 use Imagick;
@@ -30,248 +30,276 @@ require_once 'MapStyle.php';
 class TileProxy
 {
 
-	const LOGLEVEL_OFF = 0;
-	const LOGLEVEL_INFO = 1;
-	const LOGLEVEL_DEBUG = 2;
+    const LOGLEVEL_OFF = 0;
+    const LOGLEVEL_INFO = 1;
+    const LOGLEVEL_DEBUG = 2;
 
-	private $z = 0;
-	private $x = 0;
-	private $y = 0;
-	private $user_agent = "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/74.0";
+    private $z = 0;
+    private $x = 0;
+    private $y = 0;
+    private $user_agent = "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/74.0";
 
-	private $option_storage_dir = "../cache/";
-	private $option_log_dir = "../log/";
-	private $option_loglevel = 0;
-	private $option_ttl = 86400; // default browser expiry time
-	private $option_refresh = 60; // refresh after n days
+    private $option_storage_dir = "../cache/";
+    private $option_log_dir = "../log/";
+    private $option_loglevel = 0;
+    private $option_ttl = 86400; // default browser expiry time
+    private $option_refresh = null; // refresh after n days, null = ignored
 
-	private $allow_referrer = NULL;
-	private $styles = array();
-        private $lang = null;
-
-	public function addStyle(MapStyle $style)
-	{
-		$this->styles[$style->getName()] = $style;
-	}
-
-	public function setBrowserCacheExpire($hours)
-	{
-		$this->option_ttl = $hours * 3600;
-	}
-
-	public function setRefresh($days)
-	{
-		$this->option_refresh = $days;
-	}
-
-	public function setCacheDir($option_storage_dir)
-	{
-		$this->$option_storage_dir = $option_storage_dir;
-	}
-
-	public function setLogDir($option_log_dir)
-	{
-		$this->$option_log_dir = $option_log_dir;
-	}
+    private $allow_referrer = NULL;
+    private $styles = array();
+    private $lang = null;
 
 
-        public function setLang($lang)
+    public function addStyle(MapStyle $style)
+    {
+        $this->styles[$style->getName()] = $style;
+    }
+
+    public function setBrowserCacheExpire($hours)
+    {
+        $this->option_ttl = $hours * 3600;
+    }
+
+    public function setRefresh($days)
+    {
+        $this->option_refresh = $days;
+    }
+
+
+
+    public function setCacheDir($option_storage_dir)
+    {
+        $this->$option_storage_dir = $option_storage_dir;
+    }
+
+    public function setLogDir($option_log_dir)
+    {
+        $this->$option_log_dir = $option_log_dir;
+    }
+
+    public function setLang($lang)
+    {
+        $this->$lang = $lang;
+    }
+
+    public function setReferrer($referrer)
+    {
+        $this->allow_referrer = $referrer;
+    }
+
+    public function setLogLevel($loglevel)
+    {
+        $this->option_loglevel = $loglevel;
+    }
+
+
+    private function log($msg, $level = 0)
+    {
+        if($level <= $this->option_loglevel) {
+            file_put_contents($this->option_log_dir."/proxy.log", date("Y M j G:i:s", time())." ".$msg."\n", FILE_APPEND);
+        }
+    }
+
+    private function is_valid_image($path, $current_style)
+    {
+        $a = getimagesize($path);
+        $image_type = $a[2];
+        if(in_array($image_type , array($current_style->getImageChecktype())))
         {
-                $this->$lang = $lang;
+            return true;
+        }
+        return false;
+    }
+
+
+    private function fetchTile($current_style_name, $filepath, $target_dir)
+    {
+        $success = false;
+        // choose a random mirror
+        $current_style = $this->styles[$current_style_name];
+        $random = rand(0, count($this->styles[$current_style_name]->getMirrors()) -1);
+        $domain = $this->styles[$current_style_name]->getMirrors()[$random];
+        $filepath = str_replace(".webp", ".png", $filepath);
+
+        $url = $domain . $filepath;
+        if(!is_null($current_style->getLang())) {
+            $url = $domain . $filepath. "?lang=".$current_style->getLang();
+        }
+        $this->log("Downloading ".$url, 2);
+        $save_to = "{$this->option_storage_dir}/{$current_style_name}{$filepath}";
+        $this->log("Saving to ".$save_to, 2);
+        $this->log("mkdir ".$target_dir, 2);
+        if(!is_dir($target_dir)) {
+            mkdir($target_dir, 0777, true);
+        }
+        // tile download
+        $ch = curl_init($url);
+        $fp = fopen($save_to, "w");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch,CURLOPT_USERAGENT, $this->user_agent);
+        curl_exec($ch);
+        curl_close($ch);
+        fflush($fp);
+        fclose($fp);
+
+        // check if image downloaded successfully
+        if($this->is_valid_image($save_to, $current_style)) {
+            $success = true;
+
+            if($current_style->getImageFormat() != MapStyle::IMAGE_FORMAT_PNG) {
+                $this->log("to ". $current_style->getImageFormat(), 2);
+                $save_to = $this->changeImageFormat($save_to, $current_style->getImageFormat());
+            }
+
+            // TODO: dont save image in between
+            if($current_style->getModulate() || $current_style->getSepia() || $current_style->getNegate()) {
+                $image_obj = new Imagick(realpath($save_to));
+                if($current_style->getModulate()) {
+                    $this->log("Modulating Image", 2);
+                    $this->modulateImage($image_obj, $current_style->getModulateBrightness(), $current_style->getModulateSaturation(), $current_style->getModulateHue());
+                }
+
+                if($current_style->getSepia()) {
+                    $this->log("Sepia Image", 2);
+                    $this->sepiaToneImage($image_obj, $current_style->getSepiaValue());
+                }
+
+                if($current_style->getNegate()) {
+                    $this->log("Negate Image", 2);
+                    $this->negateImage($image_obj, $current_style->getNegateGrayOnly(), $current_style->getNegateChannel());
+                }
+                $image_obj->writeImage($save_to);
+            }
+
+        } else {
+            unlink($save_to);
         }
 
 
-	public function setReferrer($referrer)
-	{
-		$this->allow_referrer = $referrer;
-	}
+        return $success;
+    }
 
-	public function setLogLevel($loglevel)
-	{
-		$this->option_loglevel = $loglevel;
-	}
+    /**
+     *  image processing
+     **/
+    function changeImageFormat($target_file, $format) {
+        $imagick = new Imagick($target_file);
+        $imagick->setImageFormat($format);
+        $new_target_file = str_replace(".png", ".".$format, $target_file);
+        $this->log("Converting to " . $new_target_file, 2);
+        unlink($target_file);
+        $imagick->writeImage($new_target_file);
+        return $new_target_file;
+    }
 
+    function sepiaToneImage($image_obj, $sepia) {
+        //$imagick = new Imagick(realpath($imagePath));
+        return $image_obj->sepiaToneImage($sepia);
+        //$imagick->writeImage($imagePath);
+    }
 
-	private function log($msg, $level = 0)
-	{
-		if($level <= $this->option_loglevel) {
-			file_put_contents($this->option_log_dir."/proxy.log", date("Y M j G:i:s", time())." ".$msg."\n", FILE_APPEND);
-		}
-	}
+    function modulateImage($image_obj, $brightness, $saturation, $hue) {
+        //$imagick = new Imagick($target_file);
+        return $image_obj->modulateImage($brightness, $saturation, $hue);
+        //$imagick->writeImage($target_file);
+    }
 
-	private function is_valid_image($path)
-	{
-		$a = getimagesize($path);
-		$image_type = $a[2];
-		if(in_array($image_type , array(IMAGETYPE_PNG)))
-		{
-			return true;
-		}
-		return false;
-	}
+    function negateImage($image_obj, $grayOnly, $channel = Imagick::CHANNEL_DEFAULT) {
+        //$imagick = new Imagick(realpath($imagePath));
+        return $image_obj->negateImage($grayOnly, $channel);
+        //$imagick->writeImage($imagePath);
+    }
 
-
-	private function fetchTile($current_style_name, $filepath, $target_dir)
-	{
-		$success = false;
-		// choose a random mirror
-		$current_style = $this->styles[$current_style_name];
-		$random = rand(0, count($this->styles[$current_style_name]->getMirrors()) -1);
-		$domain = $this->styles[$current_style_name]->getMirrors()[$random];
-
-		$url = $domain . $filepath;
-                if(!is_null($current_style->getLang())) {
-                   $url = $domain . $filepath. "?lang=".$current_style->getLang();
+    /**
+     *
+     **/
+    private function validate($parts)
+    {
+        $valid = true;
+        if(!is_null($this->allow_referrer)) {
+            if (!empty($_SERVER['HTTP_REFERER'])) {
+                if($_SERVER['HTTP_REFERER'] != $this->allow_referrer) {
+                    $valid = false;
+                    $this->log("referrer_not_allowed: ". $_SERVER['HTTP_REFERER'], 1);
                 }
-		$this->log("Downloading ".$url, 2);
-		$save_to = "{$this->option_storage_dir}/{$current_style_name}{$filepath}";
-		$this->log("Saving to ".$save_to, 2);
-		$this->log("mkdir ".$target_dir, 2);
-		if(!is_dir($target_dir)) {
-			mkdir($target_dir, 0777, true);
-		}
-		// tile download
-		$ch = curl_init($url);
-		$fp = fopen($save_to, "w");
-		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch,CURLOPT_USERAGENT, $this->user_agent);
-		curl_exec($ch);
-		curl_close($ch);
-		fflush($fp);
-		fclose($fp);
+            }
+        }
 
-		// check if image downloaded successfully
-		if($this->is_valid_image($save_to)) {
-			$success = true;
+        if(count($parts) != 5) {
+            $this->log("invalid request url", 1);
+            $valid = false;
+        }
 
-			// TODO: dont save image in between
-			if($current_style->getModulate()) {
-				$this->log("Modulating Image", 2);
-				$this->modulateImage($save_to, $current_style->getModulateBrightness(), $current_style->getModulateSaturation(), $current_style->getModulateHue());
-			}
+        if(!$this->styles[$parts[1]]) {
+            $valid = false;
+            $this->log("invalid style requested", 1);
+        }
 
-			if($current_style->getSepia()) {
-				$this->log("Sepia Image", 2);
-				$this->sepiaToneImage($save_to, $current_style->getSepiaValue());
-			}
+        return $valid;
+    }
 
-			if($current_style->getNegate()) {
-				$this->log("Negate Image", 2);
-				$this->negateImage($save_to, $current_style->getNegateGrayOnly(), $current_style->getNegateChannel());
-			}
+    public function handle()
+    {
+        $path_only = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $parts = explode("/", $path_only);
 
-		} else {
-			unlink($save_to);
-		}
+        if($this->validate($parts)) {
+            $succes = false;
 
+            $current_style = $this->styles[$parts[1]];
+            $current_style_name = $current_style->getName();
 
-		return $success;
-	}
+            $target_file = "/${parts[2]}/${parts[3]}/{$parts[4]}";
+            $target_dir = "{$this->option_storage_dir}/{$current_style_name}/${parts[2]}/${parts[3]}/";
+            $check_file = "{$this->option_storage_dir}/{$current_style_name}/${parts[2]}/${parts[3]}/{$parts[4]}";
 
-	/**
-	 *  image processing
-	 **/
-	function sepiaToneImage($imagePath, $sepia) {
-		$imagick = new Imagick(realpath($imagePath));
-		$imagick->sepiaToneImage($sepia);
-		$imagick->writeImage($imagePath);
-	}
+            $this->log("Checking ". $check_file, 2);
+
+            if (!is_file($check_file))
+            {
+                $success = $this->fetchTile($current_style_name, $target_file, $target_dir);
+            } else {
+                // check if refresh is needed
+                // if no refresh is set, we can skip this operation
+                if(!is_null($this->option_refresh)) {
+                    if(filemtime($check_file) > time() - (86400 * $this->option_refresh) ) {
+                        $this->log("refresh needed.", 2);
+                        $success = $this->fetchTile($current_style_name, $target_file, $target_dir);
+                    } else {
+                        $this->log("file found in cache.", 2);
+                        $success = true;
+                    }
+                } else {
+                    $success = true;
+                }
+            }
 
 
-	function modulateImage($target_file, $brightness, $saturation, $hue) {
-		$imagick = new Imagick($target_file);
-		$imagick->modulateImage($brightness, $saturation, $hue);
-		$imagick->writeImage($target_file);
-	}
+            // we set broser cache options
+            if($success) {
+                $exp_gmt = gmdate("D, d M Y H:i:s", time() + $this->option_ttl) ." GMT";
+                $mod_gmt = gmdate("D, d M Y H:i:s", filemtime($check_file)) ." GMT";
+                header("Expires: " . $exp_gmt);
+                header("Last-Modified: " . $mod_gmt);
+                header("Cache-Control: public, max-age=" . $this->option_ttl);
+                header ('Content-Type: image/'.$current_style->getImageFormat());
+                readfile($check_file);
+                flush();
+            } else {
+                // something else went wrong
+                header ('Content-Type: text/html');
+                http_response_code(404);
+            }
 
-	function negateImage($imagePath, $grayOnly, $channel = Imagick::CHANNEL_DEFAULT) {
-		$imagick = new Imagick(realpath($imagePath));
-		$imagick->negateImage($grayOnly, $channel);
-		$imagick->writeImage($imagePath);
-	}
-
-	private function validate($parts)
-	{
-		$valid = true;
-		if(!is_null($this->allow_referrer)) {
-			if (!empty($_SERVER['HTTP_REFERER'])) {
-				if($_SERVER['HTTP_REFERER'] != $this->allow_referrer) {
-					$valid = false;
-					$this->log("referrer_not_allowed: ". $_SERVER['HTTP_REFERER'], 1);
-				}
-			}
-		}
-
-		if(count($parts) != 5) {
-			$this->log("invalid request url", 1);
-			$valid = false;
-		}
-
-		if(!$this->styles[$parts[1]]) {
-			$valid = false;
-			$this->log("invalid style requested", 1);
-		}
-
-		return $valid;
-	}
-
-	public function handle()
-	{
-		$path_only = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-		$parts = explode("/", $path_only);
-
-		if($this->validate($parts)) {
-			$succes = false;
-
-			$current_style = $this->styles[$parts[1]];
-			$current_style_name = $current_style->getName();
-
-			$target_file = "/${parts[2]}/${parts[3]}/{$parts[4]}";
-			$target_dir = "{$this->option_storage_dir}/{$current_style_name}/${parts[2]}/${parts[3]}/";
-			$check_file = "{$this->option_storage_dir}/{$current_style_name}/${parts[2]}/${parts[3]}/{$parts[4]}";
-
-			$this->log("Checking ". $check_file, 2);
-
-			if (!is_file($check_file))
-			{
-				$success = $this->fetchTile($current_style_name, $target_file, $target_dir);
-			} else {
-				// check if refresh is needed
-				if(filemtime($check_file) > time() - (86400 * $this->refresh) ) {
-					$this->log("refresh needed.", 2);
-					$success = $this->fetchTile($current_style_name, $target_file, $target_dir);
-				} else {
-					$this->log("file found in cache.", 2);
-					$success = true;
-				}
-			}
+        } else {
+            // image not found
+            header ('Content-Type: text/html');
+            http_response_code(404);
+        }
 
 
-			// we set broser cache options
-			if($success) {
-				$exp_gmt = gmdate("D, d M Y H:i:s", time() + $this->option_ttl) ." GMT";
-				$mod_gmt = gmdate("D, d M Y H:i:s", filemtime($check_file)) ." GMT";
-				header("Expires: " . $exp_gmt);
-				header("Last-Modified: " . $mod_gmt);
-				header("Cache-Control: public, max-age=" . $this->option_ttl);
-				header ('Content-Type: image/png');
-				readfile($check_file);
-				flush();
-			} else {
-				// something else went wrong
-				header ('Content-Type: text/html');
-				http_response_code(404);
-			}
-
-		} else {
-			// image not found
-			header ('Content-Type: text/html');
-			http_response_code(404);
-		}
-
-
-	}
+    }
 
 
 }
